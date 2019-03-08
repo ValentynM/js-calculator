60aa482c7686c736bacb1c0a707465742c51e1bc
Valentyn Malenchak <valentyn17@gmail.com>
2019-03-08 13:19:35 +0000

    Some bugs fixed

--- js/index.js
+++ js/index.js
@@ -4,21 +4,26 @@ document.querySelectorAll('.digits button, .operations button').forEach(button =
     button.addEventListener('click', digitOrOperationClicked));
 
 function digitOrOperationClicked(event) {
-    display.value += event.target.innerText;
+    display.value = getNewExpression(display.value, event.target.innerText);
 }
 
 document.querySelector('.equal').addEventListener('click', equalPressed);
 document.querySelector('.clear').addEventListener('click', clearDisplay);
+document.querySelector('.history button').addEventListener('click', clearHistory);
 
 function clearDisplay() {
     display.value = '';
 }
 
+function clearHistory() {
+    notes.textContent = '';
+}
+
 function equalPressed() {
+    // на випадок якщо чогось не врахував
     try {
-        const result = eval(display.value);
+        const result = eval(display.value.split('^').join('**'));
         logCalculation(display.value, result);
-        console.log(previousCalculations);
         display.value = result;
     }
     catch (SyntaxError) {
@@ -28,10 +33,36 @@ function equalPressed() {
     }
 }
 
-const calcHistory = document.querySelector('.history');
+const historyBlock = document.querySelector('.history');
+const notes = document.createElement('p');
 let previousCalculations = [];
 
 function logCalculation(expr, result) {
-    previousCalculations.unshift(`${expr} = ${result}`);
+    previousCalculations.unshift(`${expr}=${result}`);
     previousCalculations.length > 10 ? previousCalculations.pop() : null;
+    notes.textContent = previousCalculations.join("\r\n");
+    historyBlock.appendChild(notes);
+}
+
+function getNewExpression(expression, newCharacter) {
+    const digits = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']);
+    const operations = new Set(['+', '-', '*', '/', '^']);
+
+    if (expression == '' && ['+', '*', '/', '^'].includes(newCharacter)) {
+        return expression;
+    }
+
+    if (expression.slice(-1) == '.' && ['+', '-', '*', '/', '^'].includes(newCharacter)) {
+        return expression;
+    }
+
+    if ((operations.has(newCharacter)) && (operations.has(expression.slice(-1)))) {
+        if (expression.slice(0, -1)) {
+            return expression.slice(0, -1) + newCharacter;
+        }
+        else {
+            return 0;
+        }
+    }
+    return expression + newCharacter;
 }
